ff75d812e9f48869b7e96764126c6d1e0fea434c
DorvilleThiago <thiago.dorville@souunit.com>
2023-05-31 10:28:00 -0300

    fix: send, img, get

--- src/index.js
+++ src/index.js
@@ -21,46 +21,62 @@ const pool = new Pool({
 
 const queryFotos = (fotos) => {
   if (fotos.length === 1) {
-    return "(pedido_key, blob_one) VALUES ($1, $2)";
+    return "(pedido_key, blob_one) VALUES ($1, $2);";
   } else if (fotos.length === 2) {
-    return "(pedido_key, blob_one, blob_two) VALUES ($1, $2, $3)"
+    return "(pedido_key, blob_one, blob_two) VALUES ($1, $2, $3);"
   } else if (fotos.length === 3) { 
-    return "(pedido_key, blob_one, blob_two, blob_three) VALUES ($1, $2, $3, $4)"
+    return "(pedido_key, blob_one, blob_two, blob_three) VALUES ($1, $2, $3, $4);"
   }
 }
 
 app.post('/create', async(req, res) => {
+
   const { nome, quantidade, detalhes, fotos } = req.body
-  console.log(JSON.stringify(fotos))
+
   let lista_de_fotos = []
+
   for (foto of fotos) { 
-    lista_de_fotos.push(foto)
+    lista_de_fotos.push(foto.blob)
   }
+
   if (!nome || !quantidade || !detalhes || !fotos) {
     return res.status(400).json({ error: "tá faltando nome, quantidade ou detalhes..." }).send()
   }
-  await pool.query(
+  
+  const result = await pool.query(
     `INSERT INTO pedidos (nome, quantidade, detalhes) VALUES ($1, $2, $3) RETURNING id;`,
-    [nome, quantidade, detalhes],
-    (error, results) => {
-      if (error) { console.log(error) }      
-      const insertedId = results.rows[0].id;
-      lista_de_fotos.unshift(insertedId)
-    })
-  console.log('foi um')
-  await pool.query(
-    `INSERT INTO fotos ${queryFotos(fotos)}`,
-    lista_de_fotos,
-    (error, results) => {
-      console.log('dentro da query')
-      if (error) { console.log(lista_de_fotos); console.log(error) }
-    })
-  res.status(200).json({ message: "deu certo!" })
-  console.log('foi dois')
+    [nome, quantidade, detalhes])
+  
+  const insertedId = result.rows[0].id;
+  lista_de_fotos.unshift(insertedId)
+
+  try {
+    console.log("INSERT INTO fotos "+queryFotos(fotos))
+    await pool.query(
+      `INSERT INTO fotos ${queryFotos(fotos)}`,
+      [...lista_de_fotos])
+    res.status(200).json({ message: "deu certo!" })
+  } catch (err) {
+    console.log(err)
+  }
 })
 
 app.get('/pedidos', (req, res) => {
-    pool.query('SELECT * FROM pedidos;', (error, results) => {
+  pool.query(`
+  SELECT
+  P.id,
+  P.nome,
+  P.quantidade,
+  P.detalhes,
+  F.blob_one,
+  F.blob_two,
+  F.blob_three
+  FROM 
+    pedidos P
+  INNER JOIN 
+    fotos F
+  ON 
+  P.id = F.pedido_key;`, (error, results) => {
         if (error) {
             throw error
         }
